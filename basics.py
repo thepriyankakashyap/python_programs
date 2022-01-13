7772651d591adb47d186aba353655983265b80c0
Priyanka K <54153547+thepriyankakashyap@users.noreply.github.com>
2022-01-14 03:45:00 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,87 @@
+# print (" Hello world " )
+
+# print ("priyanka")
+
+# i = 10
+# print ( i )
+
+# print (" i = ", i)
+
+# a, b, c = 10, 9, 7
+
+# print (" a= ", a)
+# print (" b= ", b)
+# print (" c= ", c)
+
+# print ("a=", a ,"b=", b, "c=", c)
+
+# p = g = y = "gukgkf" 
+
+# print("p=",p ,"g=", g ,"y=", y)
+
+
+# q = 6 
+# f = 7
+
+# sum= q+f
+# print( "sum=", sum )
+
+# mul= q*f
+# print( "mul=", mul )
+
+# sub= q-f
+# print("sub=" , sub )
+
+# div = q/f
+# print("div=" , div )
+
+# s = input("enter a value ")
+# print("s=",s)
+
+# print(type(s))
+
+# i = int(input("enter an integer "))
+# print(i)
+# print(type(i))
+# h = float(input("enter a float "))
+# print(h)
+# print(type(h))
+# k = str(input("enter a string "))
+# print(type(k))
+
+# p = int(input("enter a value "))
+# print(type(p))
+# t = int(input("enter a value "))
+# print(type(t))
+
+# p = input("enter a value ")
+# p = int(p)
+# print(type(p))
+# t = input("enter a value ")
+# t = int(t)
+# print(type(t))
+
+
+# sum= p+t
+# print("sum=", sum) 
+# print(type(sum))
+
+# mul= p*t
+# print("mul=", mul)
+# print(type(mul))
+
+# sub= p-t
+# print("sub=", sub)
+# print(type(sub))
+
+# div= p/t
+# print("div=", div)
+# print(type(div))
+
+# i= 0
+# k= 90
+# print(i, k, "hello", "subham bohut besi petla hoi and lazy harami hoi")
+
+# print(i, k, sep='$' )
+
+# print(i, k, sep='%', end='_')
